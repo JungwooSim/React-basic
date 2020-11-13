069959436ac0029c4e2ec6d25528e8f51bcd86c7
JungwooSim <cscd053@gmail.com>
2020-11-13 09:03:24 +0900

    State Update

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0,
-    };
-
     handleIncrement = () => {
-        this.setState({count: this.state.count + 1})
-    };
+        this.props.onIncrement(this.props.habit);
+    }
+    handleDecrement = () => {
+        this.props.onDecrement(this.props.habit);
+    }
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    }
 
-    handleDecrement  = () => {
-        const count = this.state.count - 1;
-        this.setState({count: count < 0 ? 0 : count })
-    };
     render() {
-        console.log(this.props.habit);
         const { name, count } = this.props.habit;
         return (
             <li className="habit">
@@ -26,7 +23,9 @@ class Habit extends Component {
                 <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
                     <i className="fas fa-minus-square"/>
                 </button>
-                <button className="habit-button habit-delete">
+                <button className="habit-button habit-delete"
+                        onClick={this.handleDelete}
+                >
                     <i className="fas fa-trash"/>
                 </button>
             </li>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -9,12 +9,36 @@ class Habits extends Component {
             { id: 3, name: 'Coding', count: 0},
         ]
     }
+
+    handleIncrement = (habit) => {
+        const habits = [...this.state.habits]; // habits를 복제
+        const index = habits.indexOf(habit); // 매게변수로 받은 habit의 index를 넣음
+        habits[index].count++;
+        this.setState({habits}); // 수정된 habits를 다시 셋팅
+    };
+
+    handleDecrement  = (habit) => {
+        const habits = [...this.state.habits]; // habits를 복제
+        const index = habits.indexOf(habit); // 매게변수로 받은 habit의 index를 넣음
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({habits}); // 수정된 habits를 다시 셋팅
+    };
+    handleDelete = (habit) => {
+        const habits = this.state.habits.filter(item => item.id !== habit.id); // habits를 반복하며 매게변수로 받은 habit 이 아닌것만 골라서 habits에 넣음
+        this.setState({habits}); // 그리고 habits를 재설정
+    };
     render() {
         return (
             <ul>
                 {
                     this.state.habits.map(habit => (
-                        <Habit key={habit.id} habit={habit}/>
+                        <Habit key={habit.id}
+                               habit={habit}
+                               onIncrement={this.handleIncrement}
+                               onDecrement={this.handleDecrement}
+                               onDelete={this.handleDelete}
+                        />
                     ))
                 }
             </ul>
