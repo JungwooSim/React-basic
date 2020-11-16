e0c1a2c7d5129ff5abd297b288ebcea52f066299
JungwooSim <cscd053@gmail.com>
2020-11-16 20:27:32 +0900

    react router

---
+++ router/src/components/profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+const Profile = (props) => {
+    const history = useHistory();
+    return (
+        <>
+            <h1>Profile</h1>
+            <button onClick={() => {
+                history.push('home');
+            }}>Go to Profile</button>
+        </>
+    );
+}
+
+export default Profile;
